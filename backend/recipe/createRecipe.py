d0bae4c96d35c107a701d398442f46378518bdf2
MettBrotDev <florentin@familiedoll.net>
2024-07-03 11:19:02 +0200

    Upgraded llm

--- backend/recipe/createRecipe.py
+++ backend/recipe/createRecipe.py
@@ -4,21 +4,20 @@ import numpy as np
 
 class Model:
     def __init__(self):
-        self.model = transformers.pipeline("text2text-generation", model="flax-community/t5-recipe-generation")
+        model_id = "MettBrot/flan-t5-small-quaso-gen3"
+        self.model = transformers.pipeline("text2text-generation", model=model_id, tokenizer=model_id)
 
     def __createRecipe(self, text):
         return self.model(text)
     
     def getRecipe(self, prompt):
         output = self.__createRecipe(prompt)
-
-        output = output[0]['generated_text'].split('directions:')[1]
         return output
     
 def createPrompt(recipeName, ingredients, meanOfSamples):
-    prompt = f"Title: {recipeName}; Ingredients:"
+    prompt = f"Create a recipe for: Title {recipeName} Ingredients: "
     meanOfSamples = np.round(meanOfSamples, 2)
     for i, ingredient in enumerate(ingredients):
-        prompt += f" {ingredient}," if i != len(ingredients) - 1 else f" {ingredient}."
+        prompt += f"{ingredient}, " if i != len(ingredients) - 1 else f" {ingredient}."
         # {meanOfSamples[i]}
     return prompt
